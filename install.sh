0c53b5b3a79d11271baa73125c6695ac15ce6ce2
k33la1n0 <75585954+k33la1n0@users.noreply.github.com>
2021-01-08 14:35:30 +0100

    Update install.sh
    Fixed a bug in the installation script where wrong loops and queries were deleted, further the status output was improved

--- install.sh
+++ install.sh
@@ -5,21 +5,20 @@ echo "> ffmpeg"
 echo "> v4l2loopback-dkms"
 echo "> v4l-utils"
 echo "-----------------------------------"
-read -p "Are you sure? " -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
+read -p "Are you sure? y for yes | n for no " -n 1 -r
 sudo apt install ffmpeg -y
 sudo apt install v4l2loopback-dkms -y
 sudo apt install v4l-utils -y
-fi
-echo ""
 echo "-----------------------------------"
-read -p "Would you like install a virtual webcam? " -n 1 -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
+echo "Installation completed"
+echo ""
 sudo chmod +x start.sh
+echo "-----------------------------------"
+echo "Execute permissions were passed to start.sh"
+echo ""
 sudo modprobe v4l2loopback video_nr=5 card_label=“vcam”
-fi
+echo "-----------------------------------"
+echo "Virtual webcam number 5 is set up"
 echo ""
 echo "-----------------------------------"
 echo "Your Virtual Cam is named as vcam"
